e1e4e3ba7bbf8d105054661e4f5e22f24edaa136
olv-leo <1996olv.leo@gmail.com>
2022-04-22 12:41:56 -0300

    Add a dependencia do Python decouple

--- pypro/settings.py
+++ pypro/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u@au)ov-%vj5rc^#dku!flt_a(%zds-qa^p62xivd00-fzf1ym'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
