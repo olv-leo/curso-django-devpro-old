174a9db15299197fa3fad1f2b1c53a087eb62439
olv-leo <1996olv.leo@gmail.com>
2022-04-26 14:14:53 -0300

    Configurando coleta de arquivos estáticos

--- pypro/settings.py
+++ pypro/settings.py
@@ -119,8 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# Configuração de ambiente de desenvolvimento
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
