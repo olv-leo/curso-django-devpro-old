9dd62b498bc448d2c178101154e1310f7233d6df
olv-leo <1996olv.leo@gmail.com>
2022-04-28 11:05:26 -0300

    Separando as urls da app

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -6,7 +6,7 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client: Client):
-    return client.get(reverse('home'))
+    return client.get(reverse('base:home'))
 
 
 def test_status_code(resp):
@@ -18,4 +18,4 @@ def test_title(resp):
 
 
 def test_home_link(resp):
-    assert_contains(resp, f'href="{reverse("home")}">PythonPro</a>')
+    assert_contains(resp, f'href="{reverse("base:home")}">PythonPro</a>')
