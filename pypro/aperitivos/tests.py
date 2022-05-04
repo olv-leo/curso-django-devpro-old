59aeb69ac275b2119caab55c9d29e26326edcaf3
olv-leo <1996olv.leo@gmail.com>
2022-05-04 12:15:24 -0300

    Criando infraestrutura para novo app aperitivos com TDD

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -1,11 +1,22 @@
 import pytest
 from django.urls import reverse
 
+from pypro.django_assertions import assert_contains
+
 
 @pytest.fixture
 def resp(client):
     return client.get(reverse('aperitivos:video', args=('motivacao',)))
 
-#Testando se o acesso a página está acontecendo com sucesso.
+
+# Testando se o acesso a página está acontecendo com sucesso.
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_title_video(resp):
+    assert_contains(resp, '<h1>Vídeo Aperitivo: Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/705098707?h=a5468669ed"')
