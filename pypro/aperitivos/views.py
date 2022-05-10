bd999656ded60a86e935804246b111044e0ad65d
olv-leo <1996olv.leo@gmail.com>
2022-05-10 12:29:20 -0300

    Trabalhando com contexto nos vídeos do templates do app aperitivos

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+        'apresentacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': '705098707',
+                         'page_title': 'Python Pro - Aperitivo'},
+        'feynman': {'titulo': 'Aula 01 - Técnica Feynman', 'vimeo_id': '708273691',
+                    'page_title': 'Python Pro - Feynman'},
+    }
+    video = videos[slug]
+
+    return render(request, 'aperitivos/video.html', context={'video': video})
